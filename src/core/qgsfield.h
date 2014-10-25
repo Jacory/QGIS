6749557a96b902b53774b6d8745c72603af51a8a
Jacory <jacorygao@outlook.com>
2014-10-25 22:12:56 +0800

    nothing

--- src/core/qgsfield.h
+++ src/core/qgsfield.h
@@ -27,7 +27,7 @@
 
 class CORE_EXPORT QgsField
 {
-  public:
+public:
     /** Constructor. Constructs a new QgsField object.
      * @param name Field name
      * @param type Field variant type, currently supported: String / Int / Double
@@ -39,25 +39,25 @@ class CORE_EXPORT QgsField
      * used in conjunction with other fields types (eg. variable character fields)
      * @param comment Comment for the field
      */
-
+    
     QgsField( QString name = QString(),
               QVariant::Type type = QVariant::Invalid,
               QString typeName = QString(),
               int len = 0,
               int prec = 0,
               QString comment = QString() );
-
+              
     //! Destructor
     ~QgsField();
-
+    
     bool operator==( const QgsField& other ) const;
-
+    
     //! Gets the name of the field
     const QString & name() const;
-
+    
     //! Gets variant type of the field as it will be retrieved from data source
     QVariant::Type type() const;
-
+    
     /**
       Gets the field type. Field types vary depending on the data source. Examples
       are char, int, double, blob, geometry, etc. The type is stored exactly as
@@ -65,81 +65,81 @@ class CORE_EXPORT QgsField
       @return QString containing the field type
      */
     const QString & typeName() const;
-
-
+    
+    
     /**
       Gets the length of the field.
       @return int containing the length of the field
      */
     int length() const;
-
-
+    
+    
     /**
       Gets the precision of the field. Not all field types have a related precision.
       @return int containing the precision or zero if not applicable to the field type.
      */
     int precision() const;
-
+    
     /**
     Returns the field comment
     */
     const QString & comment() const;
-
+    
     /**
       Set the field name.
       @param nam Name of the field
      */
     void setName( const QString & nam );
-
+    
     /**
       Set variant type.
      */
     void setType( QVariant::Type type );
-
+    
     /**
       Set the field type.
       @param typ Field type
      */
     void setTypeName( const QString & typ );
-
+    
     /**
       Set the field length.
       @param len Length of the field
      */
     void setLength( int len );
-
+    
     /**
       Set the field precision.
       @param prec Precision of the field
      */
     void setPrecision( int prec );
-
-
+    
+    
     /**
       Set the field comment
       */
     void setComment( const QString & comment );
-
-  private:
+    
+private:
 
     //! Name
     QString mName;
-
+    
     //! Variant type
     QVariant::Type mType;
-
+    
     //! Type name from provider
     QString mTypeName;
-
+    
     //! Length
     int mLength;
-
+    
     //! Precision
     int mPrecision;
-
+    
     //! Comment
     QString mComment;
-
+    
 }; // class QgsField
 
 // key = field index, value=field data
